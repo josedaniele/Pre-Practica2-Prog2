cb37da528a2537cfe32bfe91877d691d1aada9d7
josedaniele <josedaniele493@gmail.com>
2022-08-23 11:42:37 -0300

    primer ejercicio

--- PrePractica2_Ej_1.py
+++ PrePractica2_Ej_1.py
@@ -3,6 +3,18 @@
 #Nota : Hacerlo con la función max(a,b) y luego con una comparación
 
 #INICIO
+lista=[]
+a= int(input("Ingrese el valor de a "))
+lista.append(a)
+b= int(input("Ingrese el valor de b "))
+lista.append(b)
+max_value = max(lista)
+print("El maximo valor es :" ,max_value)
 
+if lista[0]> lista[1] :
+    max_value =lista[0]
+else:
+    max_value= lista[1]
 
+print("El maximo valor es :" ,max_value)
 #FIN
