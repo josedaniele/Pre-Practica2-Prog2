ae71f59aeae141c2ef44f5a35a1dd4ccf0e8d30f
josedaniele <josedaniele493@gmail.com>
2022-08-23 11:50:21 -0300

    ejercicio 2

--- PrePractica2_Ej_2.py
+++ PrePractica2_Ej_2.py
@@ -3,5 +3,14 @@
 #El programa debe de mostrar por pantalla la lista de numeros originales y la lista de numeros impares.
 
 #INICIO
+lista = []
+listaImpar = []
+for i in range(3) :
+    valor=int(input("Ingrese un numero: "))
+    lista.append(valor)
+    if valor%2 != 0:
+     listaImpar.append(valor)  
 
+print("La lista entera tiene los siguientes valores :",lista)
+print("Los valores impares son :",listaImpar)   
 #FIN
